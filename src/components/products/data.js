4c9404ab00179d8ff55f7d44ca282abfbe2088af
Bob F <bobfdev@gmail.com>
2022-01-14 23:24:43 -0600

    completed files

--- src/components/products/data.js
+++ src/components/products/data.js
@@ -1,6 +1,9 @@
 import product1 from '../../images/product-1.jpeg';
 import product2 from '../../images/product-2.jpeg';
 import product3 from '../../images/product-3.jpeg';
+import sweet1 from '../../images/sweet3.jpeg';
+import sweet2 from '../../images/sweet-2.jpeg';
+import sweet3 from '../../images/sweet-3.jpeg';
 
 
 export const productData = [
@@ -27,5 +30,32 @@ export const productData = [
         desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
         price: '$19.99',
         button: 'Add to Cart',
+    },
+]
+
+export const productDataTwo = [
+    {
+        img: sweet2,
+        alt: 'Donuts',
+        title: 'Doughlicious',
+        desc: 'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+        price: '$9.99',
+        button: 'Add To Cart',
+    },
+    {
+        img: sweet3,
+        alt: 'Ice Cream',
+        title: 'Caramel Wonder',
+        desc: 'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+        price: '$9.99',
+        button: 'Add To Cart',
+    },
+    {
+        img: sweet1,
+        alt: 'Brownie',
+        title: 'Brownie Bunch',
+        desc: 'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+        price: '$9.99',
+        button: 'Add To Cart',
     },
 ]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import Hero from '../components/hero';
 import Products from '../components/products';
-import { productData } from '../components/products/data';
+import Feature from '../components/feature';
+import Footer from '../components/footer';
+import { productData, productDataTwo } from '../components/products/data';
 
 function Home() {
     return (
         <div>
             <Hero />
             <Products heading='Choose your favorite' data={productData} />
+            <Feature />
+            <Products heading='Sweet Treats for You' data={productDataTwo} />
+            <Footer />
         </div>
     )
 }
